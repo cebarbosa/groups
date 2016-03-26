98661ca3d6d7c7125dca5fb0be0ba00aeb687a59
Carlos Barbosa <kadu.barbosa@gmail.com>
2016-03-26 15:00:28 -0300

    New file to make maps of the observations.

---
+++ obs_maps.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on 26/03/16
+
+@author: Carlos Eduardo Barbosa
+
+Make images of groups and members
+
+"""
+import os
+
+import numpy as np
+import pyfits as pf
+
+from config import *
+
+if __name__ == "__main__":
+    wdir = os.path.join(data_dir, )
