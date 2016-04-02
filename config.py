cb756ca03678be64b522c9aee73b57a350c97af9
Carlos Barbosa <kadu.barbosa@gmail.com>
2016-04-02 18:47:42 -0300

    Inclusion of the coordinates of the groups.

--- config.py
+++ config.py
@@ -27,6 +27,10 @@ v0s = dict([("hcg22", 2698.), ("hcg62", 4413.), ("hcg90", 2638.),
 
 # Defining systems
 groups = ["hcg22", "hcg42", "hcg62", "hcg90", "ngc193", "ngc7619"]
+rac = ["03h03m31.3s", "10h00m21.8s", "12h53m05.5s", "22h02m05.6s",
+       "00h39m16.4s", "23h19m42.9s"]
+decc = ["-15d40m32s", "-19d38m57s", "-09d12m01s", "-31d58m00s", "+03d17m04s",
+       "+08d09m45s"]
 
 # Defining observing runs
 nights = ["blanco10n1", "blanco10n2", "blanco11an1", "blanco11an2",
